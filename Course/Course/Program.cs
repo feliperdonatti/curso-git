dd67612b12817010a30f10911e59315c78779c55
Felipe Renan Donatti <feliperdonatti@gmail.com>
2020-07-28 23:50:30 -0300

    Novo Projeto Course

---
+++ Course/Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Delivered
+
+            };
+
+            Console.WriteLine(order);
+
+            //Converter string para emun
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+
+            string txt = OrderStatus.Delivered.ToString();
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+
+        }
+    }
+}

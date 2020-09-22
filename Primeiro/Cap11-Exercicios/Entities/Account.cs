d95a9e1551e3fefc21d7b0d3b528b53afb1e3c73
Felipe Donatti <feliperdonatti@gmail.com>
2020-09-22 07:42:07 -0300

    Arquivos Cao13 do Curso

---
+++ Primeiro/Cap11-Exercicios/Entities/Account.cs
@@ -0,0 +1,50 @@
+﻿using Cap11_Exercicios.Entities.Exception;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap11_Exercicios.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("O valor precisa ser maior que zero");
+            }
+
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+
+            }
+
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ Primeiro/Cap13-TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cap13_TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Aula5Exemplo1();
+        }   
+
+        public void Aula5Exemplo1()
+        {
+            string path = @"c:\temp\file1.txt";
+
+            Console.WriteLine("FileName: " + Path.GetFileName(path));
+            Console.WriteLine("PathSeparator:" + Path.PathSeparator);
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path) );
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+
+        public void Aula4Exemplo1()
+        {
+            try
+            {
+                string sourcePath = @"c:\temp\";
+
+                IEnumerable<string> folders = Directory.EnumerateDirectories(sourcePath, "*.*", SearchOption.AllDirectories);
+
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                var files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Directory.CreateDirectory(sourcePath + "\\newFolder");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Aula3Exemplo1()
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (var line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void Aula2Exemplo1()
+        {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }catch(IOException e)
+            {
+
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+
+        public void Aula2Exemplo2()
+        {
+            string path = @"c:\temp\file1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                
+            }
+            catch (IOException e)
+            {
+
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+
+        public void Aula2Exemplo3()
+        {
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            Console.WriteLine(sr.ReadLine());
+                        }
+                    }
+                }
+            } catch(IOException e)
+            {
+
+            }
+        }
+
+
+        public void Aula1()
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file3.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    
+    }
+}
